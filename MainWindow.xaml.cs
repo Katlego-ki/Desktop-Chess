fc1a942489d913bd9f1b7a288fb348e6a3d09b5b
Katlego-ki <katlegobairvin@gmail.com>
2025-06-30 22:08:03 +0200

    use a uniformGrid for the chessboard

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Desktop_Chess.Model;
+using Desktop_Chess.View;
 
 namespace Desktop_Chess
 {
@@ -21,7 +22,12 @@ namespace Desktop_Chess
     {
         private Point StartPoint {  get; set; } //check
         private Piece? DraggedPiece { get; set; } //check
-        private TranslateTransform Transform = new(); //check
+        private readonly TranslateTransform Transform = new(); //check
+        readonly static UniformGrid chessBoard = new()
+        {
+            Rows = 8,
+            Columns = 8,
+        };
 
         public MainWindow()
         {
@@ -31,18 +37,7 @@ namespace Desktop_Chess
 
         private void CreateChessBoard()
         {
-
-            UniformGrid chessBoard = new()
-            {
-                Rows = 8,
-                Columns = 8
-            };
-
-
-            //create a piece
-            Piece? piece;
-            string? type = null;
-            string? source =null;
+            this.Content = chessBoard;
 
             for (int row = 0; row < 8; row++)
             {
@@ -53,74 +48,82 @@ namespace Desktop_Chess
                         Background =  new ImageBrush(new BitmapImage(new Uri($"pack://application:,,,/Assets/other/{((row + col) % 2 != 0 ?"ds.png":"ls.png")}")))
                     };
 
-                    // ======== PAWN ========
-                    if(row == 1 || row == 6)
-                    {
-                        type = "pawn";
-                        source = row == 6 ? "wp.png" : "bp.png";   
-                    }
+                    var piece = Add_Piece(row, col);
 
-                    // ======== ROOK ========
-                    if ((row == 0 || row == 7) && (col == 0 || col == 7))
+                    //if a piece was added
+                    if (piece != null)
                     {
-                        type = "rook";
-                        source = row == 7 ? "wr.png" : "br.png";
-                    }
-
-                    // ======== KNIGHT ========
-                    if ((row == 0 || row == 7) && (col == 1 || col == 6))
-                    {
-                        type = "knight";
-                        source = row == 7 ? "wn.png" : "bn.png";
-                    }
-
-                    // ======== KING ========
-                    if ((row == 0 || row == 7) && col == 4)
-                    {
-                        type = "king";
-                        source = row == 7 ? "wk.png" : "bk.png";
-                    }
-
-                    // ======== BISHOP ========
-                    if ((row == 0 || row == 7) && (col == 2 || col == 5))
-                    {
-                        type = "bishop";
-                        source = row == 7 ? "wb.png" : "bb.png";
-                    }
-
-                    // ======== QUEEN ========
-                    if ((row == 0 || row == 7) && col == 3)
-                    {
-                        type = "queen";
-                        source = row == 7 ? "wq.png" : "bq.png";   
-                    }
-
-
-                    //If there is a piece to be added
-                    if (source != null && type != null)
-                    {
-                        piece = new(source, type);
                         Control_Piece(piece);
                         square.Child = piece;
-                        type = null;
                     }
 
                     Panel.SetZIndex(square, 0);
-                    Add_Square_To_Grid(square, row, col); 
+                    chessBoard.Children.Add(square);
                 }
             }
         }
 
-        private void Add_Square_To_Grid(Border square, int row, int col) //will be removed when UniformGrid is used
+        static Piece?  Add_Piece(int row, int col)
         {
-            chessBoardGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50) });
-            chessBoardGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(50) });
+            string? type = null;
+            string? source = null;
 
-            //species where to add the piece i.e. " <sqaure Grid.Row = row Grid.Column = column /> 
-            square.SetValue(Grid.RowProperty, row);
-            square.SetValue(Grid.ColumnProperty, col);
-            chessBoardGrid.Children.Add(square);
+            // ======== PAWN ========
+            if (row == 1 || row == 6)
+            {
+                type = "pawn";
+                source = row == 6 ? "wp.png" : "bp.png";
+            }
+
+            // ======== ROOK ========
+            if ((row == 0 || row == 7) && (col == 0 || col == 7))
+            {
+                type = "rook";
+                source = row == 7 ? "wr.png" : "br.png";
+            }
+
+            // ======== KNIGHT ========
+            if ((row == 0 || row == 7) && (col == 1 || col == 6))
+            {
+                type = "knight";
+                source = row == 7 ? "wn.png" : "bn.png";
+            }
+
+            // ======== KING ========
+            if ((row == 0 || row == 7) && col == 4)
+            {
+                type = "king";
+                source = row == 7 ? "wk.png" : "bk.png";
+            }
+
+            // ======== BISHOP ========
+            if ((row == 0 || row == 7) && (col == 2 || col == 5))
+            {
+                type = "bishop";
+                source = row == 7 ? "wb.png" : "bb.png";
+            }
+
+            // ======== QUEEN ========
+            if ((row == 0 || row == 7) && col == 3)
+            {
+                type = "queen";
+                source = row == 7 ? "wq.png" : "bq.png";
+            }
+
+            //If there is a piece to be added
+            return (source != null && type != null)? new(source, type) : null;
         }
+
+        //private void Add_Square_To_Grid(Border square, int row, int col) //will be removed when UniformGrid is used
+        //{
+        //    chessBoardGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50) });
+        //    chessBoardGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(50) });
+
+        //    //species where to add the piece i.e. " <sqaure Grid.Row = row Grid.Column = column /> 
+        //    square.SetValue(Grid.RowProperty, row);
+        //    square.SetValue(Grid.ColumnProperty, col);
+        //    chessBoardGrid.Children.Add(square);
+        //}
 
         private void Control_Piece(Piece piece)
         {
@@ -131,7 +134,7 @@ namespace Desktop_Chess
 
         private void Piece_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            StartPoint = e.GetPosition(chessBoardGrid);
+            StartPoint = e.GetPosition(chessBoard);
             DraggedPiece = (Piece)sender;
 
             var parent = (Border)VisualTreeHelper.GetParent(DraggedPiece);
@@ -158,13 +161,13 @@ namespace Desktop_Chess
             {
                 DraggedPiece.ReleaseMouseCapture();
 
-                Point dropPosition = e.GetPosition(chessBoardGrid);
+                Point dropPosition = e.GetPosition(chessBoard);
 
-                foreach( UIElement element in chessBoardGrid.Children)
+                foreach( UIElement element in chessBoard.Children)
                 {
                     if(element is Border border) //element selected by cursor is border (square)
                     {
-                        Point sqaurePosition = border.TranslatePoint(new Point(0, 0), chessBoardGrid);
+                        Point sqaurePosition = border.TranslatePoint(new Point(0, 0), chessBoard);
                         Rect sqaureRect = new(sqaurePosition.X, sqaurePosition.Y, border.ActualWidth, border.ActualHeight);
                         var capturedPiece = (Piece) border.Child;
                         var parent = (Border)VisualTreeHelper.GetParent(DraggedPiece);
